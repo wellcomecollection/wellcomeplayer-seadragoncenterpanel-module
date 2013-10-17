9265008159f0f2e19fdb2e146f7d1e268b2b92e3
unknown <e.silverton@gmail.com>
2013-10-17 16:12:28 +0100

    removed css vars

--- seadragonCenterPanel.js
+++ seadragonCenterPanel.js
@@ -1,5 +1,3 @@
-/// <reference path="../../js/jquery.d.ts" />
-/// <reference path="../../js/extensions.d.ts" />
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -33,7 +31,6 @@ define(["require", "exports", "../coreplayer-seadragoncenterpanel-module/seadrag
         };
 
         SeadragonCenterPanel.prototype.overlaySearchResults = function () {
-            // loop through entries to get those for the current index.
             var page = null;
             var searchResults = (this.app).searchResults;
 
@@ -71,7 +68,6 @@ define(["require", "exports", "../coreplayer-seadragoncenterpanel-module/seadrag
                 var w = rect.w;
                 var h = rect.h;
 
-                // normalise into seadragon points.
                 var factor = 1 / sourceWidth;
                 var xp = factor * x;
                 var yp = factor * y;
@@ -89,4 +85,3 @@ define(["require", "exports", "../coreplayer-seadragoncenterpanel-module/seadrag
     })(baseCenter.SeadragonCenterPanel);
     exports.SeadragonCenterPanel = SeadragonCenterPanel;
 });
-//# sourceMappingURL=seadragonCenterPanel.js.map
